5457fd920daf76e06503345fe58315a04c468c75
Luca Llanos <llanosluca1998@gmail.com>
2021-07-12 05:40:22 -0300

    CSS settings #3

--- src/views/Info/Info.js
+++ src/views/Info/Info.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import './Info.css';
+import React from "react";
+import "./Info.css";
 
 function Info() {
-    return(
-        <div className='text'>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, aliquid quo. Quia voluptate quidem nulla minima illo soluta, fugiat repellat, beatae voluptas sint mollitia aliquam quae veniam sapiente magni harum?
-            Quos quae voluptatem, quam culpa nemo quasi perferendis labore quo, ipsa nisi laboriosam esse ex assumenda maiores ea provident error impedit distinctio reprehenderit ipsam. Nemo maxime dolorem inventore nulla harum.
-            Modi rerum dolore aspernatur ducimus cumque. Tempora voluptate, mollitia, libero autem a est officiis consequuntur, at laboriosam optio tempore animi ex fugiat amet fugit ipsam vero earum aperiam quod ipsa.</p>
-        </div>
-    )
+  return (
+    <div className="text">
+      <h1>Info</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, aliquid
+        quo. Quia voluptate quidem nulla minima illo soluta, fugiat repellat,
+        beatae voluptas sint mollitia aliquam quae veniam sapiente magni harum?
+        Quos quae voluptatem, quam culpa nemo quasi perferendis labore quo, ipsa
+        nisi laboriosam esse ex assumenda maiores ea provident error impedit
+        distinctio reprehenderit ipsam. Nemo maxime dolorem inventore nulla
+        harum. Modi rerum dolore aspernatur ducimus cumque. Tempora voluptate,
+        mollitia, libero autem a est officiis consequuntur, at laboriosam optio
+        tempore animi ex fugiat amet fugit ipsam vero earum aperiam quod ipsa.
+      </p>
+    </div>
+  );
 }
 
 export default Info;
